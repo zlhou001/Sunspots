8074037a84989809f46a851feb552f0c3bd6cc41
zlhou001 <zulei_hou@hotmail.com>
2016-06-08 09:44:28 +0800

    Update main.py
    change for Python version

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from urllib import urlopen
+from urllib.request import urlopen
 from reportlab.graphics.shapes import *
 from reportlab.graphics.charts.lineplots import LinePlot
 from reportlab.graphics.charts.textlabels import Label
